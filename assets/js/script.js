054975274687febce8e61b70ded191ffac4dcb8d
rinklepoonia <rinklepoonia889@gmail.com>
2024-01-11 15:41:18 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+menuIcon.addEventListener("click", function () {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overFlowHidden")
+    menuIcon.classList.toggle('cross')
+})
